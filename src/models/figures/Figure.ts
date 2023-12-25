234dc449836134e51d86ec12668841000ba7e53c
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-25 13:25:07 +0200

    create classes figures

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,34 @@
+import { Colors } from './../Colors';
+import { Cell } from '../Cell';
+import logo from '../../assets/black-king.png';
+
+export enum FigureNames{
+  FIGURE = 'Фигура',
+  KING = 'Король',
+  KNIGHT = 'Конь',
+  PAWN = 'Пешка',
+  QUEEN = 'Ферзь',
+  ROOK = 'Ладья',
+  BISHOP = 'Слон',
+}
+
 export class Figure{
-    
+ color: Colors;
+ logo: typeof logo | null;
+ cell: Cell;
+ name: FigureNames;
+ id: number;
+
+ constructor(color:Colors, cell:Cell){
+    this.color = color;
+    this.logo = null;
+    this.cell = cell;
+    this.name = FigureNames.FIGURE;
+    this.id = Math.random();
+ }
+
+ canMove(target: Cell): boolean{
+    return true;
+ }
+moveFigure(target: Cell){}
 }
