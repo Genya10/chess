b9d9f3c030044089afdbe6b382acbdccfe61b583
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-25 09:15:16 +0200

    add main styles and create Board, Cell components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BoardComponent } from './components/BoardComponent';
 
 function App() {
   return (
     <div className="App">
- Chess
+      <BoardComponent/>
     </div>
   );
 }
