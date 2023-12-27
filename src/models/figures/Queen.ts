2ad3b008376a1ce895b5c244b8ebd92be2891ba8
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-27 09:03:47 +0200

    add canMove to figure-classes

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -11,4 +11,9 @@ constructor(color:Colors,cell:Cell){
     this.logo = color === Colors.BLACK ? blackLogo: whiteLogo;
     this.name = FigureNames.QUEEN;
  }
+ canMove(target: Cell): boolean {
+    if(!super.canMove(target))
+    return false;
+   return true;
+}
 }
