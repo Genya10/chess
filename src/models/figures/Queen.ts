ba0ae216af27afeaf6056cf9c4297fb06a9e7cd4
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-27 10:28:11 +0200

    create methods isEmpty,...Vertical,Horizontal,Diag

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -13,7 +13,13 @@ constructor(color:Colors,cell:Cell){
  }
  canMove(target: Cell): boolean {
     if(!super.canMove(target))
+      return false;
+    if(this.cell.isEmptyVertical(target))
+       return true;
+    if(this.cell.isEmptyHorizontal(target))
+       return true;
+       if(this.cell.isEmptyDiagonal(target))
+       return true;
     return false;
-   return true;
 }
 }
